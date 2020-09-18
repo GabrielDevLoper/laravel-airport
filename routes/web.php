48488c9949e67fc36ceee24b77335077b364cab6
Gabriel Barreto <gabriel.barret015@hotmail.com>
2020-09-18 12:09:02 -0300

    create brandcontroller and your's routes, using route::resource.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
-Route::get('/', "Site\SiteController@index")->name("home-site");
-Route::get('/panel', "Panel\PanelController@index");
+Route::group(['prefix' => 'panel', 'namespace' => 'Panel'], function () {
+    Route::resource('/brands', 'BrandController');
+    Route::get('/', "PanelController@index");
+});
 
+Route::get('/', "Site\SiteController@index");
 Route::get('/promocoes', "Site\SiteController@promotions")->name('promotions');
 
 Auth::routes();
